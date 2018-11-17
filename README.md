317d78262b3359da8bb9fad3bc078172b2122e97
Tim Speckhals <github@timmyrs.de>
2018-11-17 11:26:34 +0100

    Upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# ShutdownInMinutes
+
+A Spigot plugin to automatically shut down your server after a set amount of minutes.
+
+This is useful when using plugins such as [Varo](https://github.com/timmyrs/Varo) which create new worlds but can't delete them when the server has already loaded them.
---
+++ src/de/timmyrs/ShutdownInMinutes.java
@@ -0,0 +1,24 @@
+package de.timmyrs;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShutdownInMinutes extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		getConfig().addDefault("shutdownInMinutes", 1440L);
+		getConfig().addDefault("disconnectMessage", "The server is going down for a quick self-maintenance restart. Reconnect in ~10 seconds.");
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		reloadConfig();
+		getServer().getScheduler().scheduleSyncDelayedTask(this, ()->{
+			for(Player p : getServer().getOnlinePlayers())
+			{
+				p.kickPlayer(getConfig().getString("disconnectMessage"));
+			}
+			getServer().shutdown();
+		}, getConfig().getLong("shutdownInMinutes") * 1200L);
+	}
+}
